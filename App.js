27848352c2a98a820e71b59dff1636270c62e89b
patrickbasham <PatrickTBasham@gmail.com>
2022-08-25 18:33:53 -0400

    udpated layout for all views to display nice on screen

--- App.js
+++ App.js
@@ -7,29 +7,44 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
     return (
         <View style={styles.appContainer}>
-           <View style={styles.inputContainer}>
-            <TextInput style={styles.textInput} placeholder="Your course goal!" />
-            <Button title="Add Goad" />
-           </View>
-           <View>
-            <Text>List your goals</Text>
-           </View>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder="Your course goal!" />
+                <Button title="Add Goal" />
+            </View>
+            <View style={styles.goalsContainer}>
+                <Text>List your goals</Text>
+            </View>
         </View>
     );
 }
 
 
+/*========================================
+        React-Native Styling
+========================================*/
 const styles = StyleSheet.create({
     appContainer: {
-        padding: 50
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 15,
     },
     inputContainer: {
-         flexDirection: "row",
-         justifyContent: "space-between"
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: "#cccccc"
     },
     textInput: {
         borderWidth: 1,
         borderColor: "#cccccc",
-        width: "80%",
+        width: "70%",
+        marginRight: 8,
+        padding: 8,
+    },
+    goalsContainer: {
+        flex: 5,
     },
 });
