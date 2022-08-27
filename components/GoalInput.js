c40011bb0456de1b64548609bfc05fcc25ba5b9d
patrickbasham <PatrickTBasham@gmail.com>
2022-08-27 17:42:47 -0400

    changed styling for GoalInput Modal and child elements, added button that will eventually close modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,7 +25,14 @@ export const GoalInput = (props) => {
         <Modal visible={props.visible} animationType="slide">
             <View style={styles.inputContainer}>
                 <TextInput style={styles.textInput} placeholder="Your course goal!" value={enteredGoalText} onChangeText={goalInputHandler} />
-                <Button title="Add Goal" onPress={addGoalHandler} />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Add Goal" onPress={addGoalHandler} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Cancel" />
+                    </View>
+                </View>
             </View>
         </Modal>
     )
@@ -34,18 +41,26 @@ export const GoalInput = (props) => {
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
+        flexDirection: "column",
+        justifyContent: "center",
         alignItems: "center",
         marginBottom: 24,
+        padding: 16,
         borderBottomWidth: 1,
         borderBottomColor: "#cccccc"
     },
     textInput: {
         borderWidth: 1,
         borderColor: "#cccccc",
-        width: "70%",
-        marginRight: 8,
+        width: "100%",
         padding: 8,
     },
+    buttonContainer: {
+        marginTop: 16,
+        flexDirection: "row",
+    },
+    button: {
+        marginHorizontal: 8,
+        width: 100,
+    }
 });
